dac2b9f939d563bbf78ec9522f3fe6405dc7cea8
pennybelle <contact@pennybelle.dev>
2025-03-08 18:26:04 -0500

    create run_server

--- dman_functional.py
+++ dman_functional.py
@@ -17,14 +17,24 @@
 # 	fi
 # }
 
-from os import path, makedirs
+import os
+import subprocess
+import asyncio
+# import shlex
+# from os import path, makedirs
 from subprocess import Popen, PIPE
 
-steamcmd = path.join("app", "steamcmd")
-servers = path.join("app", "servers")
+# import pwd
+# username = pwd.getpwuid(os.getuid())[0]
+# dman_path = os.path.join("/", "home", username, "Documents", "GitHub", "dman")
+# steamcmd = os.path.join(dman_path, "app", "steamcmd")
+# servers = os.path.join(dman_path, "app", "servers")
+
+steamcmd = os.path.join(os.getcwd(), "app", "steamcmd")
+servers = os.path.join(os.getcwd(), "app", "servers")
 
 
-# run any command (pipe or not)
+# used for downloading steamcmd
 def command(input):
     output = Popen(
         input,
@@ -32,35 +42,66 @@ def command(input):
         stdout=PIPE,
         stderr=PIPE,
     )
-    output = str(output.communicate()[0])
-    output = output[2 : len(output) - 3]
+    output = output.communicate()
 
     return output
 
 
+# def split_args(command):
+#     return shlex.split(command)
+
+
 def install_steamcmd():
-    print("Checking for steamcmd...")
-    if path.isdir(steamcmd) is not True:
-        print("No steamcmd found, installing...", end="", flush=True)
-        makedirs(steamcmd)
+    print("checking for steamcmd...")
+    if os.path.isdir(steamcmd) is not True:
+        print("no steamcmd found, installing...", end="", flush=True)
+        os.makedirs(steamcmd)
         command(f'cd {steamcmd} && curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -')
-        print("Done")
-        print("Steamcmd installed")
+        print("done")
+        print("steamcmd installed")
     else:
-        print("Steamcmd found")
+        print("steamcmd found")
 
 
-def create_instance(name, username):
-    print("Creating new server instance")
-    instance_path = path.join("app", "servers", name)
-    if path.isdir(instance_path) is not True:
-        makedirs(instance_path)
-        command(f"{path.join(steamcmd, "steamcmd.sh")} +force_install_dir {path.join(servers, name)} +login {username} +app_update 223350 +quit")
+def check_server(server_name):
+    print(f"looking for server instance {server_name}")
+    instance_path = os.path.join(servers, server_name)
+    if os.path.isdir(instance_path) is not True:
+        print("no server by that name, creating...")
+        # os.makedirs(instance_path)
+        username = input("enter your steam username: ")
+        subprocess.run([f'{os.path.join(steamcmd, "steamcmd.sh")}', f"+force_install_dir {instance_path}", f"+login {username}", "+app_update 223350", "+quit"], shell=False)
+        print(f"server instance {server_name} has been created")
+        # print(server)
+    else:
+        print(f"server instance {server_name} has been found")
+
+
+def run_server(server_name):
+    instance_path = os.path.join(servers, server_name)
+    subprocess.run([
+        os.path.join(instance_path, "./DayZServer"), 
+        f'-config={os.path.join(instance_path, "serverDZ.cfg")}', 
+        "-port=2301", 
+        f'-BEpath={os.path.join(instance_path, "battleye")}', 
+        f'-profiles={os.path.join(instance_path, "profiles")}', 
+        "-dologs", 
+        "-adminlog", 
+        "-netlog", 
+        "-freezecheck"
+    ], cwd=instance_path)
+
+    # print(server.pid)
+
+    # command(f"cd {instance_path} && ./DayZServer -config={os.path.join(instance_path, "serverDZ.cfg")} -port=2301 -BEpath={os.path.join(instance_path, "battleye")} -profiles={os.path.join(instance_path, "profiles")} -dologs -adminlog -netlog -freezecheck")
 
 
 def main():
     install_steamcmd()
 
+    check_server("pennybelle")
+    print(os.getcwd())
+    run_server("pennybelle")
 
 
 
